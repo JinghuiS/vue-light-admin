de72ab73c5bcc3949bff73cf11c750aca36cd867
石京辉 <1292077619@qq.com>
2023-02-22 14:31:52 +0800

    feat: 🎸 add PageCard

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,12 +1,14 @@
 import type { Component } from 'vue'
 import type { Form } from 'vee-validate'
 import FormItem from './form/FormItem.vue'
+import PageCard from './pageCard/PageCard.vue'
 import { tableComponentList, BaseTable } from './table'
 
 export interface GlobalComponentListType {
     VeeFormItem: typeof FormItem
     VeeForm: typeof Form
     LightTable: typeof BaseTable
+    PageCard: typeof PageCard
 }
 
-export default [FormItem, ...tableComponentList] as Component[]
+export default [FormItem, PageCard, ...tableComponentList] as Component[]
