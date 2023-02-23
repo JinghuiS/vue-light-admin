5b8dbf7f0e4426ab0114512c4535c7fa8017145c
石京辉 <1292077619@qq.com>
2023-02-23 21:08:10 +0800

    feat: 🎸 add elementRef type

--- src/shared/utils/elementRef.ts
+++ src/shared/utils/elementRef.ts
@@ -1,5 +1,26 @@
-import { ref, unref, type Ref } from 'vue'
+import type {
+    ClassComponent,
+    GlobalComponentConstructor
+} from 'primevue/ts-helpers'
+import { ref, unref, type Component, type Ref } from 'vue'
 
+/**
+ *
+ * Component Template Refs
+ *
+ * 获取组件引用
+ */
+export function elementRef<
+    ElementRef extends GlobalComponentConstructor<any>
+>(): Ref<InstanceType<ElementRef> | null>
+
+/**
+ *
+ * HtmlElement Template Refs
+ *
+ * 获取html元素引用
+ *
+ */
 export function elementRef<ElementRef = HTMLElement | null>(): Ref<ElementRef> {
     return ref<any>(null)
 }
